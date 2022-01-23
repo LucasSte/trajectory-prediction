7a4a34adfa0a09f0c1da348403a8454eb6457bd9
LucasSte <lucas.tnagel@gmail.com>
2022-01-23 16:28:18 -0300

    Update load functions

--- dataset/load_dataset.py
+++ dataset/load_dataset.py
@@ -1,3 +1,4 @@
+import bisect
 import pickle
 import numpy as np
 import itertools
@@ -55,3 +56,35 @@ class LoadDataSet:
     def __init__(self, look_back, look_forth):
         self.look_back = look_back
         self.look_forth = look_forth
+
+    def get_ball_data(self, ball, stop_id, time_c):
+        local = ball[stop_id]
+        end = bisect.bisect_left(local['time_c'], time_c, 0, len(local['time_c']))
+        start = max(end - self.look_back, 0)
+        size = end - start
+
+        pos_x = local['x'][start:end]
+        pos_y = local['y'][start:end]
+        speed_x = local['v_x'][start:end]
+        speed_y = local['v_y'][start:end]
+        mask = [True]*self.look_back
+
+        if size < self.look_back:
+            diff = self.look_back - size
+            pos_x = np.concatenate([np.array([local['x'][0]]*diff), pos_x], axis=0)
+            pos_y = np.concatenate([np.array([local['y'][0]*diff]), pos_y], axis=0)
+            speed_x = np.concatenate([np.array([local['v_x'][0]*diff]), speed_x], axis=0)
+            speed_y = np.concatenate([np.array([local['v_y'][0]*diff]), speed_y], axis=0)
+            mask[0:diff] = [False]*diff
+
+        return np.stack([pos_x, pos_y, speed_x, speed_y]).T, np.array(mask, dtype=np.bool)
+    
+    @staticmethod
+    def get_robot_data(data, index):
+        x = data['pos']['x'][index]
+        y = data['pos']['y'][index]
+        v_x = data['speed']['x'][index]
+        v_y = data['speed']['y'][index]
+        psi = data['psi'][index]
+
+        return np.stack([x, y, v_x, v_y, psi]).T
