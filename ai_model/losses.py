9f35aadc627eab64f8aefe2da458085080fb6b67
LucasSte <lucas.tnagel@gmail.com>
2022-03-01 19:03:34 -0300

    Add Predictor file

--- ai_model/losses.py
+++ ai_model/losses.py
@@ -10,7 +10,7 @@ class SequenceLoss(tf.keras.losses.Loss):
         self.loss_2 = tf.keras.losses.MeanAbsoluteError(reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE)
 
     def call(self, y_true, y_pred):
-        return 100*(self.loss(y_true, y_pred) + self.loss_2(y_true, y_pred))
+        return 100 * (self.loss(y_true, y_pred) + self.loss_2(y_true, y_pred))
 
 
 class TestLoss:
@@ -22,3 +22,35 @@ class TestLoss:
         y_sum = np.sum(y_diff, axis=2)
         y_sum_total = np.sum(y_sum, axis=1)
         return np.mean(y_sum_total)
+
+    @staticmethod
+    def average_distance(y_true, y_pred):  # Mean average displacement
+        y_diff = y_true - y_pred
+        y_sq = np.square(y_diff)
+        y_dis = np.sqrt(np.sum(y_sq, axis=2))
+        y_dis_total = np.sum(y_dis, axis=1)
+        return np.mean(y_dis_total)
+
+    @staticmethod
+    def average_final_displacement(y_true, y_pred):  # Final average displacement
+        y_diff = y_true[:, -1, :] - y_pred[:, -1, :]
+        y_sq = np.square(y_diff)
+        y_dis = np.sqrt(np.sum(y_sq, axis=1))
+        return np.mean(y_dis)
+
+    def __call__(self, y_true, y_pred):
+        res = {
+            'MAE': TestLoss.mean_absolute_error(y_true, y_pred),
+            'AVD': TestLoss.average_distance(y_true, y_pred),
+            'AFD': TestLoss.average_final_displacement(y_true, y_pred)
+        }
+
+        self.error = res
+        return res
+
+    def print_error(self):
+        if self.error is None:
+            print("----Error not calculated----")
+        else:
+            for key, value in self.error.items():
+                print(f'{key}: {value}')
---
+++ ai_model/predictor.py
@@ -0,0 +1,128 @@
+from abc import ABC
+
+import tensorflow as tf
+from encoder import Encoder
+from decoder import DecoderInitializer, Decoder, DecoderInput, DecoderState
+from shape_checker import ShapeChecker
+
+
+class Predictor(tf.keras.Model):
+
+    def __init__(self, units, look_back, look_forth, result_dims, use_tf_function=True, forcing=True):
+        super(Predictor, self).__init__()
+        self.look_forth = look_forth
+        self.look_back = look_back
+        self.result_dims = result_dims
+        self.encoder = Encoder(units)
+        self.decoder_init = DecoderInitializer(units)
+        self.decoder = Decoder(units, result_dims)
+        self.use_tf_function = use_tf_function
+        self.shape_checker = ShapeChecker()
+        self.forcing = forcing
+
+    def _loop_step_no_forcing(self, input_pos, target_pos, enc_output, dec_state):
+        decoder_input = DecoderInput(
+            new_tokens=input_pos,
+            enc_output=enc_output
+        )
+
+        decoder_output = self.decoder(decoder_input, dec_state)
+        self.shape_checker(decoder_output.sequence, ('batch', 't'))
+        target_pos = tf.keras.layers.Reshape((self.result_dims, ))(target_pos)
+        self.shape_checker(target_pos, ('batch', 't'))
+        dec_state = DecoderState(state_h=decoder_output.state_h, state_c=decoder_output.state_c)
+
+        return dec_state, tf.keras.layers.Reshape((1, self.result_dims))(decoder_output.sequence)
+
+    def _loop_step(self, new_seq, enc_output, dec_state):
+        input_pos, target_pos = new_seq[:, 0:1, :], new_seq[:, 1:2, :]
+        dec_state, seq = self._loop_step_no_forcing(input_pos, target_pos, enc_output, dec_state)
+
+        return dec_state, seq
+
+    def _train_step(self, data):
+        x, y = data
+
+        with tf.GradientTape() as tape:
+            enc_output = self.encoder(x)
+
+            dec_state = self.decoder_init(DecoderState(state_h=enc_output.state_hb, state_c=enc_output.state_cb))
+            loss = tf.constant(0.0)
+
+            dec_state, seq = self._loop_step(tf.stack([x[:, self.look_back-1, 2:(4+self.use_psi)], y[:, 0:]], axis=1),
+                                             enc_output.state_h, dec_state)
+            if self.forcing:
+                for t in range(1, self.look_forth):
+                    new_seq = y[:, (t-1):(t+1), :]
+                    dec_state, point = self._loop_step(new_seq, enc_output.state_h, dec_state)
+                    seq = tf.concat([seq, point], axis=1)
+                loss = self.loss(y, seq)
+            else:
+                input_seq = seq
+                for t in range(1, self.look_forth):
+                    target_seq = y[:, (t-1):t, :]
+                    dec_state, point = self._loop_step_no_forcing(input_seq, target_seq, enc_output.state_h, dec_state)
+                    input_seq = point
+                    seq = tf.concat([seq, point], axis=1)
+                loss = self.loss(y, seq)
+
+        variables = self.trainable_variables
+        gradients = tape.gradient(loss, variables)
+        self.optimizer.apply_gradients(zip(gradients, variables))
+
+        return {'batch_loss': loss}
+
+    @tf.function
+    def _tf_train_step(self, data):
+        return self._train_step(data)
+
+    def train_step(self, data):
+        self.shape_checker = ShapeChecker()
+        if self.use_tf_function:
+            return self._tf_train_step(data)
+        else:
+            return self._train_step(data)
+
+    def save_model(self, name):
+        self.save_weights(name, save_format='tf')
+
+    def load_model(self, name):
+        self.load_weights(name).expect_partial()
+
+    def _forecast(self, input_seq):
+        enc_output = self.encoder(input_seq)
+        new_tokens = input_seq[:, (self.look_back-1):self.look_back, 2:4]
+        dec_state = self.decoder_init(DecoderState(state_h=enc_output.state_hb, state_c=enc_output.state_cb))
+
+        result_tokens = []
+        for _ in range(self.look_forth):
+
+            dec_input = DecoderInput(
+                new_tokens=new_tokens, enc_output=enc_output.state_h
+            )
+            dec_output = self.decoder(dec_input, dec_state)
+            dec_state = DecoderState(
+                state_h=dec_output.state_h,
+                state_c=dec_output.state_c,
+            )
+            new_tokens = tf.expand_dims(dec_output.sequence, axis=1)
+            result_tokens.append(dec_output.sequence)
+
+        result_tokens = tf.stack(result_tokens, axis=1)
+        return result_tokens
+
+    @tf.function
+    def _tf_forecast(self, input_seq):
+        return self._forecast(input_seq)
+
+    def forecast(self, input_seq):
+        if self.use_tf_function:
+            return self._tf_forecast(input_seq)
+        else:
+            return self._forecast(input_seq)
+
+    def call(self, inputs, training=None, mask=None):
+        if self.use_tf_function:
+            return self._tf_forecast(inputs)
+
+        return self._forecast(inputs)
